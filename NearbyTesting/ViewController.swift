c01593cfa64419f9de02ce911b551614c5f52f6f
Parker Donat <parkerdonat@gmail.com>
2016-03-24 15:26:41 -0600

    Succesfully added current location

--- NearbyTesting/ViewController.swift
+++ NearbyTesting/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet var mapView: MKMapView!
     let annotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
         
         self.mapView.delegate = self
         
@@ -165,6 +173,22 @@ class ViewController: UIViewController, MKMapViewDelegate {
             let region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
             mapView.setRegion(region, animated: true)
         }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        mapView.setRegion(region, animated: false)
+        locationManager.stopUpdatingLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        
+        
+        
     }
 }
 
